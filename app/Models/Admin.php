4e87d6d54a625e8560ccf26423c68ad26b354cd3
UsmanOfficial <UsmanOfficialuo@gmail.com>
2021-10-17 12:00:29 +0500

    Initial Commit

---
+++ app/Models/Admin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Model
+{
+    use HasFactory;
+    use Notifiable;
+
+    protected $fillable = [
+        'name', 'email', 'password'
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+}
